2c5c97f87abd7bb0626484fab542f8eb1247e990
poosana <piixd1@gmail.com>
2021-08-05 14:35:24 +0700

    part 1

--- script.js
+++ script.js
@@ -5,9 +5,26 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+const previousAuthorHtml = author.innerHTML
+let togglestatus = false
+const search = document.getElementById('search')
+const reset = document.getElementById('reset')
+const text = document.getElementById('text')
+const previousText = text.innerHTML
+const previousArr = text.innerHTML.split(" ")
+const output = text.innerHTML.split(" ")
 
 btn_toggle.onclick = () => {
   // your code here
+  if(!togglestatus){
+    author.innerHTML = parseInt(previousAuthorHtml)+parseInt(length.value)
+    btn_toggle.innerHTML = 'Show Author'
+    togglestatus = true
+  }else{
+    author.innerHTML = previousAuthorHtml
+    btn_toggle.innerHTML = 'Show Calculation'
+    togglestatus = false
+  }
 }
 
 // more codes for Search and Reset buttons here
